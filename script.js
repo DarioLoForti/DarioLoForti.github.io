56d825c4e365ee0aef429af5ba02b752142deba8
Dario Lo Forti <lofortidario@gmail.com>
2024-05-20 16:23:37 +0200

    graphic design

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
     document.addEventListener("DOMContentLoaded", function() {
+        const text1 = "Jr Full Stack Web Developer"
+        let i1 = 0;
+        const speed1 = 50;
+
+        function typeWriter() {
+            if (i1 < text1.length) {
+                document.getElementById("typewriter").innerHTML += text1.charAt(i1);
+                i1++;
+                setTimeout(typeWriter, speed1);
+            }
+        }
+
+        typeWriter();
         // const text1 = "Sono un giovane sviluppatore che ha iniziato il suo viaggio nel mondo dello sviluppo web durante il percorso formativo presso Boolean. Questa esperienza ha acceso in me una passione ardente per la programmazione e ha alimentato la mia fame di conoscenza. Con una mente aperta e una determinazione incrollabile, sono pronto ad affrontare ogni sfida e a imparare continuamente dalle esperienze che incontrerò. Sono entusiasta di contribuire con la mia creatività e dedizione a progetti stimolanti, diventando sempre più competente nel mio percorso di sviluppo professionale.";
         // let i1 = 0;
         // const speed1 = 20; // Velocità di battitura
